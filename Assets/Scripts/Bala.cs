ac97d1ffb4deb7f3b74c829fa87c614b0b69229c
ikyan17 <ikyan17@gmail.com>
2026-07-05 19:12:44 -0600

    Update Bala.cs

--- Assets/Scripts/Bala.cs
+++ Assets/Scripts/Bala.cs
@@ -2,83 +2,16 @@ using UnityEngine;
 
 public class Bala : MonoBehaviour
 {
-
-    [SerializeField] private Transform player;
-    [SerializeField] private float speed = 3.0f;
-    [SerializeField] private float detectionRadius = 12.0f;
-
-
-
-    private Rigidbody rb;
-    private bool muerto = false;
-
-    void Start()
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float lifeTime = 3f;
+    void OnTriggerEnter(Collider collider)
     {
-        rb = GetComponent<Rigidbody>();
-
-
-        {
-            GameObject jugadorObj = GameObject.FindGameObjectWithTag("Player");
-            player = jugadorObj.transform;
-        }
-    }
-
-    void FixedUpdate()
-    {
-
-
-
-        float distancia = Vector3.Distance(transform.position, player.position);
-
-        if (distancia < detectionRadius)
+        if (collider.gameObject.CompareTag("Player"))
         {
 
-            Vector3 direccion = (player.position - transform.position);
-            direccion.y = 0;
-            direccion.Normalize();
-
-
-            rb.linearVelocity = new Vector3(direccion.x * speed, rb.linearVelocity.y, direccion.z * speed);
-
-
-            if (direccion != Vector3.zero)
-            {
-                transform.forward = direccion;
-            }
-
+            Destroy(collider.gameObject);
+            Destroy(gameObject);
         }
-        else
-        {
 
-            rb.linearVelocity = new Vector3(0f, rb.linearVelocity.y, 0f);
-        }
     }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (muerto) return;
-
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            Player jugador = collision.gameObject.GetComponent<Player>();
-
-            jugador.DamagePlayer(10f);
-
-        }
-    }
-
-
-
-    void Muerte()
-    {
-        muerto = true;
-
-        rb.linearVelocity = Vector3.zero;
-
-        gameObject.SetActive(false);
-        Destroy(gameObject, 0.5f);
-    }
-
-
-
 }
