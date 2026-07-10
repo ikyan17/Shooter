181addec57985b8b1b7e8df4a2cc027d9e990a61
ikyan17 <ikyan17@gmail.com>
2026-07-10 06:50:51 -0600

    Actualizar Bullet.cs

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,7 +4,7 @@ public class Bullet : MonoBehaviour
 {
     [HideInInspector] public bool esDelJugador = true;
 
-    // El punto de entrada ahora es súper corto y limpio
+    
     void OnTriggerEnter(Collider collider)
     {
         if (esDelJugador)
@@ -17,32 +17,28 @@ public class Bullet : MonoBehaviour
         }
     }
 
-    // ==========================================
-    // 🏹 LÓGICA DE LA BALA DEL JUGADOR
-    // ==========================================
+    
     private void ManejarImpactoJugador(Collider collider)
     {
-        // ¿Chocó con una Trampa/Shooter?
+        
         if (collider.CompareTag("Trampa"))
         {
             enemigoshooter trampa = collider.GetComponentInParent<enemigoshooter>();
             if (trampa != null) trampa.TomarDaño(10f);
 
-            Destroy(gameObject); // Rompe la bala
+            Destroy(gameObject)
         }
-        // ¿Chocó con un Enemigo Melee?
+        
         else if (collider.CompareTag("Enemy"))
         {
             EnemyController enemigo = collider.GetComponentInParent<EnemyController>();
             if (enemigo != null) enemigo.TomarDaño(10f);
 
-            Destroy(gameObject); // Rompe la bala
+            Destroy(gameObject); 
         }
     }
 
-    // ==========================================
-    // 👿 LÓGICA DE LA BALA DEL ENEMIGO
-    // ==========================================
+    
     private void ManejarImpactoEnemigo(Collider collider)
     {
         // ¿Chocó con el Jugador?
