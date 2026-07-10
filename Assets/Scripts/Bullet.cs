1dec89b39b16f9c2d26e1cd665a6cf6d0efd9545
ikyan17 <ikyan17@gmail.com>
2026-07-10 07:00:10 -0600

    Actualizar Bullet.cs
    limpieza

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,50 +4,38 @@ public class Bullet : MonoBehaviour
 {
     [HideInInspector] public bool esDelJugador = true;
 
-    
     void OnTriggerEnter(Collider collider)
     {
-        if (esDelJugador)
-        {
-            ManejarImpactoJugador(collider);
-        }
-        else
-        {
-            ManejarImpactoEnemigo(collider);
-        }
+        if (esDelJugador) ManejarImpactoJugador(collider);
+        else ManejarImpactoEnemigo(collider);
     }
 
-    
     private void ManejarImpactoJugador(Collider collider)
     {
-        
         if (collider.CompareTag("Trampa"))
         {
-            enemigoshooter trampa = collider.GetComponentInParent<enemigoshooter>();
+            enemigoshooter trampa = collider.GetComponent<enemigoshooter>();
             if (trampa != null) trampa.TomarDaño(10f);
 
-            Destroy(gameObject)
+            Destroy(gameObject);
         }
-        
         else if (collider.CompareTag("Enemy"))
         {
-            EnemyController enemigo = collider.GetComponentInParent<EnemyController>();
+            EnemyController enemigo = collider.GetComponent<EnemyController>();
             if (enemigo != null) enemigo.TomarDaño(10f);
 
             Destroy(gameObject); 
         }
     }
 
-    
     private void ManejarImpactoEnemigo(Collider collider)
     {
-        // ¿Chocó con el Jugador?
         if (collider.CompareTag("Player"))
         {
             Player jugador = collider.GetComponent<Player>();
             if (jugador != null) jugador.TakeDamage(10f);
 
-            Destroy(gameObject); // Rompe la bala enemiga
+            Destroy(gameObject); 
         }
     }
 }
